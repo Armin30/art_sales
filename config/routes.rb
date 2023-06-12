fbeb534401cdde312156618bc79edab7679f0466
Armin Plath <armin.plath@bavaresca.de>
2023-06-12 14:38:40 +0200

    hooking up to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: "products#index"
   resources :products, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
     resources :rentings, only: [ :create, :destroy, :edit, :update]
+    get 'opensea/retrieve_assets'
+    post 'opensea/create_listing'
+    post 'opensea/place_bid'
   end
   resources :rentings, only: [:index]
 end
---
+++ test/controllers/opensea_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class OpenseaControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
