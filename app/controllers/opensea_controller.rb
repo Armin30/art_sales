b87dd0ec8ec38ece1cb021ad12913ad4826cef52
Armin Plath <armin.plath@bavaresca.de>
2023-06-12 14:57:56 +0200

    some changes around heroku

--- app/controllers/opensea_controller.rb
+++ app/controllers/opensea_controller.rb
@@ -2,21 +2,21 @@ class OpenseaController < ApplicationController
   def retrieve_assets
     # Use the OpenSea library to retrieve assets
     opensea_client = OpenSea::Client.new(api_key: ENV['OPENSEA_API_KEY'])
-    assets = opensea_client.get_assets(...)
+    # assets = opensea_client.get_assets(...)
     # Process the retrieved assets as needed
   end
 
   def create_listing
     # Use the OpenSea library to create a listing
     opensea_client = OpenSea::Client.new(api_key: ENV['OPENSEA_API_KEY'])
-    listing = opensea_client.create_listing(...)
+    # listing = opensea_client.create_listing(...)
     # Handle the created listing response
   end
 
   def place_bid
     # Use the OpenSea library to place a bid
     opensea_client = OpenSea::Client.new(api_key: ENV['OPENSEA_API_KEY'])
-    bid = opensea_client.place_bid(...)
+    # bid = opensea_client.place_bid(...)
     # Handle the placed bid response
   end
 end
